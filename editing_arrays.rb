fc73f43845f780420e8855c0db88719a95f614fa
flippingflippers <citrussunshine2004@gmail.com>
2018-07-25 19:21:56 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,12 +4,12 @@
 #   "light_cyan"
 # ]
 
-rainbow_colors=["red","light_red","light_yellow","green","blue"]
+rainbow_colors=["red","orange","light_yellow","green","blue"]
 
 def change_rainbow_colors
   rainbow_colors=[]
   rainbow_colors[0]="red"
-  rainbow_colors[1]="light_red"
+  rainbow_colors[1]="orange"
   rainbow_colors[2]="light_yellow"
   rainbow_colors
 end
