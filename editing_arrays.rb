b20ce6de8b400daea9a47c2d23817d3cbfe064ca
flippingflippers <citrussunshine2004@gmail.com>
2018-07-25 19:20:18 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -16,10 +16,10 @@ end
 
 puts change_rainbow_colors
 
-# def add_colors
-#   rainbow_colors=["red","light_red","light_yellow"]
-#   rainbow_colors.push "green"
-#   rainbow_colors.push "blue"
-# end
+def add_colors
+  rainbow_colors=change_rainbow_colors
+  rainbow_colors.push "green"
+  rainbow_colors.push "blue"
+end
 
-# add_colors
+add_colors
